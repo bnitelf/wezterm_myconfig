f930d7af5dadea9e40e420e0d62c0ba86d739a15
bnitelf <bnitelf@gmail.com>
2026-08-12 20:53:33 +0700

    if use wezterm on window - add option to select powershell 5, powershell 7, cmd and add ctrl+t to new tab and show option to select powershell, cmd

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,14 +18,31 @@ config.window_frame = {
 	font_size = 12.0, -- Smaller = shorter tab bar
 }
 
+-- Check if the current OS is Windows
+if wezterm.target_triple:find("windows") then
+    -- Define the launch menu choices
+    config.launch_menu = {
+        {
+            label = 'PowerShell',
+            args = { 'powershell.exe' },
+        },
+        {
+            label = 'PowerShell 7',
+            args = { 'pwsh.exe' },
+        },
+        {
+            label = 'Window Command Prompt',
+            args = { 'cmd.exe' },
+        },
+    }
 
+    -- Default Shell
+    -- Powershell 5.1
+    config.default_prog = { "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
 
--- Default Shell
--- Powershell 5.1
-config.default_prog = { "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
-
--- Powershell 7
--- config.default_prog = { "C:\\Users\\{your_username}\\AppData\\Local\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe" }
+    -- Powershell 7
+    -- config.default_prog = { "C:\\Users\\{your_username}\\AppData\\Local\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe" }
+end
 
 config.keys = {
 	-- Copy
@@ -41,17 +58,23 @@ config.keys = {
 		action = wezterm.action.PasteFrom("Clipboard"),
     },
 	-- To Switch back-forth last 2 tab
-  {
-    key = 'Tab',
-    mods = 'CTRL',
-    action = wezterm.action.ActivateLastTab,
+    {
+        key = 'Tab',
+        mods = 'CTRL',
+        action = wezterm.action.ActivateLastTab,
     },
-  -- To allow Shift + Enter to input new line when using AI agent like Kiro-cli
-  {
-    key = "Enter",
-    mods = "SHIFT",
-    action = wezterm.action.SendString("\n"),
-  }
+    -- To allow Shift + Enter to input new line when using AI agent like Kiro-cli
+    {
+        key = "Enter",
+        mods = "SHIFT",
+        action = wezterm.action.SendString("\n"),
+    },
+    -- Enable Ctrl + t to open new tab and show options to select Powershell, cmd (on Window only)
+    {
+		key = "t",
+		mods = "CTRL",
+		action = wezterm.action.ShowLauncher,
+    },
 }
 -- add my custom wezterm config dir to lua package.path so that lua know where to find module we require(module)
 package.path = package.path
